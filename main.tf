e03847c6b39f7b19c63c5a54f5485a2092d3ee4f
Mbongeni Gumede <mbongenisphiwe24@gmail.com>
2026-05-28 16:45:55 +0200

    Add Terraform gitignore

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main_vpc.id
 
   route {
-    cidr_block = var.vpc_cidr
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
